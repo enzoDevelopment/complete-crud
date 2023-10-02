df8ced6623f8b560612d5a761472a4e9303a04d0
EnzoDevelopment <enzo.big.montano@gmail.com>
2023-10-02 15:20:39 -0300

    Adicionando crud de imovel

--- crud/src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ crud/src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -1,5 +1,8 @@
 package br.com.belval.crud.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +13,9 @@ import br.com.belval.crud.model.Produto;
 @Controller
 public class ProdutoController {
 	
+	private static List<Produto> lista = new ArrayList<>();
+	private static int proxId = 1;
+	
 	@GetMapping("/produto/novo")
 	public String novo() {
 		return "produto/produto-novo";
@@ -19,7 +25,17 @@ public class ProdutoController {
 	public ModelAndView novo (Produto produto) {
 		ModelAndView model = new ModelAndView("produto/novo-produto-criado");
 		// Inserindo no banco de dados..
+		produto.setId(proxId++);
+		lista.add(produto);
+		
 		model.addObject("produto", produto);
+		return model;
+	}
+	
+	@GetMapping("/produto/lista")
+	public ModelAndView lista() {
+		ModelAndView model = new ModelAndView("produto/lista-produtos");
+		model.addObject("produtos", lista);
 		return model;
 	}
 	
